0e7f5ebdc35e5cea26e0f2a017b08f7dd9451288
AlexPashin <alexp280898@gmail.com>
2021-11-22 01:06:22 +0400

    fix 1 lab

--- source/lab1.py
+++ source/lab1.py
@@ -1,3 +1,11 @@
 import cv2
 
 print(cv2.getBuildInformation())
+
+img = cv2.imread("assets/taxi-1932107_640.jpg")
+
+cv2.namedWindow("Image")
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
