1efe384d5eed6b9ce1d5d0097c5f83f64c30ec80
AlexPashin <alexp280898@gmail.com>
2021-12-06 00:55:32 +0400

    update 4 lab

--- source/lab4.py
+++ source/lab4.py
@@ -29,6 +29,14 @@ def canny(img, level):
     return cv2.Canny(img, level, 255)
 
 
+def make_thresh(img):
+    for i in range(20000):
+        x = np.random.randint(0, img.shape[0])
+        y = np.random.randint(0, img.shape[1])
+        img[x][y] = 255
+    return img
+
+
 def sobel(img, level):
     lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
     l, a, b = cv2.split(lab)
@@ -39,8 +47,55 @@ def sobel(img, level):
     return np.uint8(cv2.threshold(abs_sobel_x + abs_sobel_y, level, 255, cv2.THRESH_BINARY)[1])
 
 
+# Коррозия
+def erode_morph(img, level):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (level, level))
+    return cv2.erode(img, kernel)
+
+
+# Открытая операция
+def open_morph(img, level):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (level, level))
+    return cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+
+# Закрытая операция
+def close_morph(img, level):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (level, level))
+    return cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+
+# Градиент
+def grad_morph(img, level):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (level, level))
+    return cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+
+
+# Черная шляпа
+def blackhat_morph(img, level):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (level, level))
+    return cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
+
+
+def connect_contour(img, level):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (level, level))
+    dilated = cv2.dilate(img, kernel)
+    _, cnts, _ = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(img, [cnts[0]], -1, (0, 255, 0), 2)
+    return img
+
+
 image = cv2.imread(".\\..\\assets\\caps.jpg")
 
 show_img("Original", image)
-show_img("Canny", canny(image, 100))
-show_img("Sobel", sobel(image, 100))
+# show_img("Canny", canny(image, 100))
+# show_img("Sobel", sobel(image, 100))
+# show_img("Eroded morph", erode_morph(image, 10))
+# show_img("Open morph", open_morph(image, 10))
+# show_img("Close morph", close_morph(image, 10))
+# show_img("Gradient morph", grad_morph(image, 10))
+# show_img("Blackhat morph", blackhat_morph(image, 10))
+# show_img("Thresh image", make_thresh(image))
+# show_img("No thrash image", open_morph(image, 5))
+show_img("Connect contour", canny(open_morph(image, 20), 200))
+
